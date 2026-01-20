2a7f0f87fbbaa9f08f7f4c50f9893815b00f66cd
qolbyadha14 <qolbyadha04@gmail.com>
2026-01-20 20:37:44 +0700

    feat: add /health endpoint that returns a JSON status response

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		// Response JSON
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"status":  "ok",
+			"message": "API Running",
+		})
+	})
+
 	fmt.Println("Starting server on port 8080")
 
 	err := http.ListenAndServe(":8080", nil)
